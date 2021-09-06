34b85243ea3f15bcdb73bab98cf6521cd5567052
hannah <hannahnajwa@gmail.com>
2021-09-06 21:54:46 +0800

    filter invoice by status

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,17 +4,20 @@
     <div class="header flex">
       <div class="left flex flex-column">
         <h1>Invoices</h1>
-        <span>There are 4 total invoices</span>
+        <span>There are {{ invoiceData.length }} total invoices</span>
       </div>
       <div class="right flex">
         <div @click="toggleFilterMenu" class="filter flex">
-          <span>Filter by status</span>
+          <span
+            >Filter by status
+            <span v-if="filteredInvoice">: {{ filteredInvoice }}</span></span
+          >
           <img src="@/assets/icon-arrow-down.svg" alt="" />
           <ul v-show="filterMenu" class="filter-menu">
-            <li>Draft</li>
-            <li>Pending</li>
-            <li>Paid</li>
-            <li>Clear filter</li>
+            <li @click="filteredInvoices">Draft</li>
+            <li @click="filteredInvoices">Pending</li>
+            <li @click="filteredInvoices">Paid</li>
+            <li @click="filteredInvoices">Clear Filter</li>
           </ul>
         </div>
         <div @click="newInvoice" class="button flex">
@@ -26,9 +29,9 @@
       </div>
     </div>
     <!-- Invoices -->
-    <div v-if="invoiceData.length > 0">
+    <div v-if="filteredData.length > 0">
       <Invoice
-        v-for="(invoice, index) in invoiceData"
+        v-for="(invoice, index) in filteredData"
         :invoice="invoice"
         :key="index"
       />
@@ -52,6 +55,7 @@ export default {
   data() {
     return {
       filterMenu: null,
+      filteredInvoice: null,
     };
   },
   components: {
@@ -65,9 +69,31 @@ export default {
     toggleFilterMenu() {
       this.filterMenu = !this.filterMenu;
     },
+    filteredInvoices(e) {
+      if (e.target.innerText === 'Clear Filter') {
+        this.filteredInvoice = null;
+        return;
+      }
+      this.filteredInvoice = e.target.innerText;
+    },
   },
   computed: {
     ...mapState(['invoiceData']),
+
+    filteredData() {
+      return this.invoiceData.filter((invoice) => {
+        if (this.filteredInvoice === 'Draft') {
+          return invoice.invoiceDraft === true;
+        }
+        if (this.filteredInvoice === 'Pending') {
+          return invoice.invoicePending === true;
+        }
+        if (this.filteredInvoice === 'Paid') {
+          return invoice.invoicePaid === true;
+        }
+        return invoice;
+      });
+    },
   },
 };
 </script>
