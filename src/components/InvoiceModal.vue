e3c4ce7af406eae77646c3a2a421169a693f1bc8
hannah <hannahnajwa@gmail.com>
2021-09-06 10:28:39 +0800

    fix required error message when click on Cancel button

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -173,11 +173,15 @@
       <!-- Save/Exit -->
       <div class="save flex">
         <div class="left">
-          <button @click="closeInvoice" class="red">Cancel</button>
+          <button type="button" @click="closeInvoice" class="red">
+            Cancel
+          </button>
         </div>
         <div class="right flex">
-          <button @click="saveDraft" class="dark-purple">Save Draft</button>
-          <button @click="publishInvoice" class="purple">
+          <button type="submit" @click="saveDraft" class="dark-purple">
+            Save Draft
+          </button>
+          <button type="submit" @click="publishInvoice" class="purple">
             Create Invoice
           </button>
         </div>
